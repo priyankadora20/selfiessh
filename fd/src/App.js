ae5298f2dbebd6f38607661a03db7ee9e9cfcb0e
priyanka dora <prdora97@gmail.com>
2023-04-14 22:12:47 +0530

    commit

---
+++ fd/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from "./pages/footer";
+
+
+function App() {
+  
+  return (
+    <>
+    
+      <div className="App">
+
+      <h1> FROM OUR BLOG </h1>
+
+      <div>  </div>
+      
+
+      
+      </div>
+      <Footer />
+      
+    </>
+    
+  );
+}
+
+export default App;
---
+++ fd/src/pages/footer.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+      <>
+    <div>
+
+          <div>
+              <h3>STORE INFO</h3>
+              <p>123 London street, United Kingdom </p>
+              <p>(+012)3456 789</p>
+              <p>hello@domain.com</p>
+
+              <div>
+                  <h3>SOCIAL</h3>
+              </div>
+
+          </div>
+          
+          <div>
+              <h3>COMPANY</h3>
+
+              <ul>
+                  <li>Careers</li>
+                  <li>Business with us</li>
+                  <li>Contact us</li>
+                  <li>About us</li>
+                  <li>FAQs</li>
+              </ul>
+
+          </div>
+
+          <div>
+              <h3>Account</h3>
+              <ul>
+                  <li>My account</li>
+                  <li>My cart</li>
+                  <li>Track my order</li>
+                  <li>Checkout</li>
+                  <li>Terms of use</li>
+              </ul>
+          </div>
+
+          <div>
+              <h3>GET IN TOUCH</h3>
+              <p>Sign up to receive amazing offers & deals</p>
+              <form action="">
+               <input type="email" /> 
+               <button>SUBSCRIBE</button>   
+              </form>
+             
+          </div>
+
+
+
+            </div>
+            <div>
+                <p>Copyright @ 2023 Selfieesh. All Right Reserved.</p>
+            </div>
+            </>
+  )
+}
+
+export default Footer;
